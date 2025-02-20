74a5c0a703bca5d50d27b54c824cf15becd1de6f
Casaverde4@1 <muriloantunesdasilva264@gmail.com>
2025-02-20 18:06:12 -0300

    Adicionando arquivos

---
+++ Aula 1/Gabarito Aula 1/src/App2.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const App2 = () => {
+    <></>
+}
+ 
+
+export default App2
+
+// rafce:
+// Criação de componente
+
+// Arrow Function
+// const arrowFunction = () => {}
+
+// Nomeação de Componente:
+// PascalCase
+
+// Nomeação de Variavel, Função ...
+// camelCase
+
+// Export default, posso importar com qualquer nome e sem chaves
+// Export "sem default", so posso importar entre chaves e com nome exportado
+
+// Self closing tag 
+// Ex: <Header/>
+
+// Nomeação de Variavel, Função ...
+// camelCase
+
+// Nomeação de classes em css
+// Metodologia Bem
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+
+// Nomes compostos sao separados por 
+
+// Tag Vazia em react se chama fragment ou fragmento
+
